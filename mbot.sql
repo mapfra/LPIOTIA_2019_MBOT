30cebec8b2c5566e6555033dfdde40a4006333e0
thomas.paredes@etu.unice.fr <thomas.paredes@etu.unice.fr>
2020-04-10 11:39:53 +0200

    Sql

---
+++ mbot.sql
@@ -0,0 +1,29 @@
+CREATE TABLE utilisateur(
+   utilisateur_id INT AUTO_INCREMENT PRIMARY KEY,
+   pseudo VARCHAR(255) NOT NULL,
+   pseudo VARCHAR(255) NOT NULL
+) ;
+
+CREATE TABLE coordonnées(
+   coordonnées_id INT AUTO_INCREMENT PRIMARY KEY,
+   longitude VARCHAR(255) NOT NULL,
+   latitude VARCHAR(255)NOT NULL
+   FOREIGN KEY (temperature_id)
+     REFERENCES tasks (temperature_id)
+        
+);
+
+
+CREATE TABLE utilisateur(
+   temperature_id INT AUTO_INCREMENT PRIMARY KEY,
+   temperature DECIMAL,
+) ;
+
+
+
+
+
+
+
+
+
